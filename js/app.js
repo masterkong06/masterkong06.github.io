f0c0c40db70cbf1dc5c91875fcf6ba549764768e
Winton Wedderburn <masterkong@ubuntu-20.04-lts>
2020-10-13 17:30:40 -0400

    Used Canvas API to render the square!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
+
+
+
 $(() => {
 
+// const $canvas = $('#game_canvas'); //select the div with id = "game_canvas"
+// const $context = $canvas.getContext('2d');
+
+const $canvas = document.getElementById('game_canvas');
+const $context = $canvas.getContext('2d');
+
+
+// top line
+$context.beginPath();
+$context.strokeStyle = 'black';
+$context.moveTo(50,50);
+$context.lineTo(100, 50);
+$context.stroke();
+
+// right line
+$context.beginPath();
+$context.strokeStyle = 'black';
+$context.moveTo(100,50);
+$context.lineTo(100, 100);
+$context.stroke();
+
+// bottom line
+$context.beginPath();
+$context.strokeStyle = 'black';
+$context.moveTo(100,100);
+$context.lineTo(50, 100);
+$context.stroke();
+
+// left line
+$context.beginPath();
+$context.strokeStyle = 'black';
+$context.moveTo(50,100);
+$context.lineTo(50, 50);
+$context.stroke();
 
 });
