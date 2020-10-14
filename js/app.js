d0f1149ffd40fe0871b08e5eb9cb1b2d6ec6a8e0
Winton Wedderburn <masterkong@ubuntu-20.04-lts>
2020-10-14 02:46:39 -0400

    Can collect the first initial of a players name

--- js/app.js
+++ js/app.js
@@ -1,38 +1,70 @@
+//Ask for player names
 
+let playerOne = '';
+// console.log(playerOne);
+
+let playerTwo = '';
+// console.log(playerOne);
+
+const getPlayerInitials = () => {
+    playerOne = prompt(`Enter Player One Name`);
+    while (playerOne == '') {
+        alert(`Please enter a name`);
+        playerOne = prompt(`Enter Player One Name`);
+    }
+    if (playerOne != '' && playerOne.length > 1) {
+        console.log(playerOne.substring(0, 1));
+        // console.log(playerOne);
+    }
+    playerTwo = prompt(`Enter Player Two Name`);
+    while (playerTwo == '') {
+        alert(`Please enter a name`);
+        playerTwo = prompt(`Enter Player Two Name`);
+    }
+    if (playerTwo != '' && playerTwo.length > 1) {
+        console.log(playerTwo.substring(0, 1));
+        // console.log(playerTwo);
+    }
+
+
+};
 
 
 $(() => {
+    getPlayerInitials();
 
-//Repurposed code from programatic DOM example
+    //Repurposed code from programatic DOM example
 
-//build a grid of four boxes
-const buildGrid = () =>{
-    for (let i = 1; i <= 4; i++){
-        makeBox(i);
+    //build a grid of four boxes
+    const buildGrid = () => {
+        for (let i = 1; i <= 4; i++) {
+            makeBox(i);
 
-    }
+        }
 
-};
+    };
 
-//make a box function
-const makeBox = (num) => {
-    $box = $('<div>').addClass('box');
-    $box.attr('id', num);
-    $('.container').append($box);
-};
+    //make a box function
+    const makeBox = (num) => {
+        $box = $('<div>').addClass('box');
+        $box.attr('id', num);
+        $('.container').append($box);
+    };
 
 
-buildGrid(); //call the code to build the grid
+    buildGrid(); //call the code to build the grid
 
-//when you hover 
+    //when you hover 
 
-// EventListeners
+    // EventListeners
 
-$('div').click(function(e){
-    if(  e.offsetX < 5){
-         $(this).css('border-left', `5px solid red`);
-     }
- });
+
+    //detects the mouse is near the left of the box
+    $('div').click(function (e) {
+        if (e.offsetX < 5) {
+            $(this).css('border-left', `5px solid red`);
+        }
+    });
 
 
 
