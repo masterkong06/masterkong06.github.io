282cebb4363374720999858dcf54d21d4bf83342
Winton Wedderburn <masterkong@ubuntu-20.04-lts>
2020-10-15 15:17:43 -0400

    top right box borders can be selected

--- js/app.js
+++ js/app.js
@@ -81,34 +81,78 @@ $(() => {
     // EventListeners
 
 
-    //detects the mouse is near the left of the box
+    //detects the mouse is near the edges of the boxes
+
+    //top left box
     $('#box_tl').click(function (event) {
-        
-        if (event.offsetX < 5) {
-            $('#box_tl').css('border-left', `10px solid red`);
-            if ($(this).hasClass('container')) {
-                $(this).css('border', `0px solid white`);
+
+                if (event.offsetX < 5) {
+                    $('#box_tl').css('border-left', `10px solid red`);
+                    if ($(this).hasClass('container')) {
+                        $(this).css('border', `0px solid white`);
+                    }
+                } else if (event.offsetX > ($('#box_tl').innerWidth() - 5)){
+                    $('#box_tl').css('border-right', `10px solid red`);
+                    $('#box_tr').css('border-left', `0px solid red`);
+                } else if (event.offsetY < 5) {
+                    $('#box_tl').css('border-top', `10px solid red`);
+                } else if (event.offsetY > ($('#box_tl').innerHeight() - 5) || event.offsetY > ($('#box_tl').innerHeight() + 5)) {
+                    $('#box_tl').css('border-bottom', `10px solid red`);
+                    $('#box_bl').css('border-top', `0px solid red`);
+                    $('#box_br').css('border-top', `0px solid red`);
+
+                }
+    });
+
+    //top right box
+    $('#box_tr').click(function (event) {
+
+        if (event.offsetX < 5) { //if I click on the left side of #box_tr
+            $('#box_tr').css('border-left', `10px solid red`);//set left border property
+            $('#box_tl').css('border-right', `0px solid red`);// and right border property of #box_tl
+            if ($(this).hasClass('container')) { 
+                $(this).css('border', `0px solid white`); //set outer container border property to white
             }
-            console.log($(this));
-        } else if (event.offsetX > ($('#box_tl').innerWidth() - 5)){
-            $('#box_tl').css('border-right', `10px solid red`);
-            $('#box_tr').css('border-left', `0px solid red`);
-        } else if(event.offsetY < 5){
-            $('#box_tl').css('border-top', `10px solid red`);
-        } else if(event.offsetY > ($('#box_tl').innerHeight() - 5) || event.offsetY > ($('#box_tl').innerHeight() + 150)) {
-            $('#box_tl').css('border-bottom', `10px solid red`);
+        } else if (event.offsetX > ($('#box_tr').innerWidth() - 5)){
+            $('#box_tr').css('border-right', `10px solid red`);
+        } else if (event.offsetY < 5) {
+            $('#box_tr').css('border-top', `10px solid red`);
+        } else if (event.offsetY > ($('#box_tr').innerHeight() - 5) || event.offsetY > ($('#box_tr').innerHeight() + 15)) {
+            $('#box_tr').css('border-bottom', `10px solid red`);
             $('#box_bl').css('border-top', `0px solid red`);
             $('#box_br').css('border-top', `0px solid red`);
 
         }
-        
-        
-        
-        //else if (event.offsetX > ($(this).innerwidth - 5) || event.offsetX < ($('#box_tl').innerwidth + 10)) {
-        //     $('#box_tl').css('border-right', `10px solid red`);
-        // }
+});    
 
-    });
+
+//bottom right box
+$('#box_br').click(function (event) {
+
+    if (event.offsetX < 5) { //if I click on the left side of #box_br
+        $('#box_br').css('border-left', `10px solid red`);//set left border property
+        $('#box_bl').css('border-right', `0px solid red`);// and right border property of #box_bl
+        if ($(this).hasClass('container')) { 
+            $(this).css('border', `0px solid white`); //set outer container border property to white
+        }
+    } else if (event.offsetX > ($('#box_br').innerWidth() - 5)){
+        $('#box_br').css('border-right', `10px solid red`);
+    } else if (event.offsetY < 5) {
+        $('#box_br').css('border-top', `10px solid red`);
+    } else if (event.offsetY > ($('#box_br').innerHeight() - 5) || event.offsetY > ($('#box_br').innerHeight() + 15)) {
+        $('#box_br').css('border-bottom', `10px solid red`);
+        $('#box_bl').css('border-top', `0px solid red`);
+        $('#box_br').css('border-top', `0px solid red`);
+
+    }
+});  
+
+
+
+
+
+
+
 
     // $('#box_tr').click(function (event) {
         
