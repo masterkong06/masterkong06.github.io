ead22737a9d6ebff57989891108c9bfd66e025ec
Winton Wedderburn <masterkong@ubuntu-20.04-lts>
2020-10-15 20:32:56 -0400

    players can alternate

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 
 let playerOneInitial = '';
 let playerTwoInitial = '';
-let playerOneColor = '';
-let playerTwoColor = '';
+let playerColor = 'red';
+// let playerTwoColor = 'blue';
 let turns = 0;
+let currentPlayer = 1;
 
 const getPlayerInitials = () => {
     // playerOneInitial = prompt(`Enter Player One Name`);
@@ -28,57 +29,89 @@ const getPlayerInitials = () => {
 };
 
 // Establish a player's color
-const getPlayerColor = () => {
-    while (playerOneColor == '') {
-        alert(`Player One: Do you want to be red or blue?`);
-        playerOneColor = prompt(`Type "Red" or "Blue" to choose your color`);
-    }
-    if (playerOneColor == '') {
-        playerOneColor = prompt(`Type "Red" or "Blue" to choose your color`);
-    } else if (playerOneColor == 'Red') {
-        playerTwoColor = 'Blue';
-    } else {
-        playerTwoColor = 'Red';
-    }
-};
+// const getPlayerColor = () => {
+//     if (playerOneColor == '' ) {
+//         alert(`Player One: Do you want to be red or blue?`);
+//         playerOneColor = prompt(`Type "Red" or "Blue" to choose your color`);
+//         console.log(playerOneColor);
+//     console.log(playerTwoColor);
+//     }else if (playerOneColor == 'Red') {
+//         playerTwoColor = 'Blue';
+//         console.log(playerOneColor);
+//     console.log(playerTwoColor);
+//     } else if (playerOneColor == 'Blue') {
+//         playerTwoColor = 'Red';
+//         console.log(playerOneColor);
+//     console.log(playerTwoColor);
+//     } else {
+//         while (playerOneColor != 'Blue' || playerOneColor != 'Red'){
+//             alert(`Player One: Do you want to be red or blue?`);
+//         playerOneColor = prompt(`Type "Red" or "Blue" to choose your color`);
+//         console.log(playerOneColor);
+//     console.log(playerTwoColor);
+//         }
+//         console.log(playerOneColor);
+//     console.log(playerTwoColor);
+//     }
+    
+//     //else if (playerOneColor != 'Blue' || playerOneColor != 'Red') {
+//     //         playerOneColor = prompt(`Type "Red" or "Blue" to choose your color`);
+//     //     }
+    
+//     // if (playerOneColor == '') {
+//     //     playerOneColor = prompt(`Type "Red" or "Blue" to choose your color`);
+//     // } else if (playerOneColor == 'Red') {
+//     //     playerTwoColor = 'Blue';
+//     // } else {
+//     //     playerTwoColor = 'Red';
+//     // }
+//     console.log(playerOneColor);
+//     console.log(playerTwoColor);
+// };
 
 //function to determine whose turn it is
 //how many possible turns are there given the number of squares drawn
 
-const turnTracking = () => {
+// const turnTracking = () => {
+//     let turnNum = 12;
+//     for (i = 1; i <= turnNum; i++) {
+//         console.log(`Turns left ${i}`);
+//     }
+
+// };
+
+// End of game occurs if turnTracking() returns 12 and no player has more than 3 boxes or turnTracking() returns 12 and either player has 3 or more boxes or turnTracking() returns less than 12 and either player has 3 or more boxes
+
+const playerSwitch = () => {
+    if (currentPlayer%2 === 0){
+   currentPlayer++; 
+        playerColor = 'red'; //player 1 css for lines
+    } else {
+        currentPlayer++;
+        playerColor = 'blue'; //player 2 css for lines
+    }
+};
+
+const playerWins = () => {
+    // if child of this box_tl has 4 classes eq() has class set then win triggered. If true currentPlayer--;
 
 };
 
 
 
 $(() => {
-    // getPlayerInitials();
+    getPlayerInitials();
     // getPlayerColor();
-
-    //Repurposed code from programatic DOM example
-
-    //build a grid of four boxes
-    // const buildGrid = () => {
-    //     for (let i = 1; i <= 4; i++) {
-    //         makeBox(i);
-
-    //     }
-
-    // };
-
-    // //make a box function
-    // const makeBox = (num) => {
-    //     $box = $('<div>').addClass('box');
-    //     $box.attr('id', num);
-    //     $('.container').append($box);
-    // };
+    // turnTracking();
+    // console.log(playerOneColor, playerTwoColor);
 
 
-    // buildGrid(); //call the code to build the grid
+    
 
-    //when you hover 
 
     // EventListeners
+
+
 
 
     //detects the mouse is near the edges of the boxes
@@ -86,75 +119,108 @@ $(() => {
     //top left box
     $('#box_tl').click(function (event) {
 
-                if (event.offsetX < 5) {
-                    $('#box_tl').css('border-left', `10px solid red`);
-                    if ($(this).hasClass('container')) {
-                        $(this).css('border', `0px solid white`);
-                    }
-                } else if (event.offsetX > ($('#box_tl').innerWidth() - 5)){
-                    $('#box_tl').css('border-right', `10px solid red`);
-                    $('#box_tr').css('border-left', `0px solid red`);
-                } else if (event.offsetY < 5) {
-                    $('#box_tl').css('border-top', `10px solid red`);
-                } else if (event.offsetY > ($('#box_tl').innerHeight() - 5) || event.offsetY > ($('#box_tl').innerHeight() + 5)) {
-                    $('#box_tl').css('border-bottom', `10px solid red`);
-                    $('#box_bl').css('border-top', `0px solid red`);
-                    $('#box_br').css('border-top', `0px solid red`);
+        if (event.offsetX < 5) {
+            $('#box_tl').css('border-left', `10px solid ${playerColor}`);
+            playerSwitch();
+            console.log(currentPlayer);
+            console.log(playerColor);
+            if ($(this).hasClass('container')) {
+                $(this).css('border', `0px solid white`);
+            }
+        } else if (event.offsetX > ($('#box_tl').innerWidth() - 5)) {
+            $('#box_tl').css('border-right', `10px solid ${playerColor}`);
+            $('#box_tr').css('border-left', `0px solid ${playerColor}`);
+            playerSwitch();
+            console.log(currentPlayer);
+            console.log(playerColor);
+        } else if (event.offsetY < 5) {
+            $('#box_tl').css('border-top', `10px solid ${playerColor}`);
+            playerSwitch();
+            console.log(currentPlayer);
+            console.log(playerColor);
+        } else if (event.offsetY > ($('#box_tl').innerHeight() - 5) || event.offsetY > ($('#box_tl').innerHeight() + 5)) {
+            $('#box_tl').css('border-bottom', `5px solid ${playerColor}`);
+            $('#box_bl').css('border-top', `5px solid ${playerColor}`);
+            playerSwitch();
+            console.log(currentPlayer);
+            console.log(playerColor);
 
-                }
+        }
+        console.log($(this));
     });
 
     //top right box
     $('#box_tr').click(function (event) {
 
         if (event.offsetX < 5) { //if I click on the left side of #box_tr
-            $('#box_tr').css('border-left', `10px solid red`);//set left border property
-            $('#box_tl').css('border-right', `0px solid red`);// and right border property of #box_tl
-            if ($(this).hasClass('container')) { 
+            $('#box_tr').css('border-left', `10px solid red`); //set left border property
+            $('#box_tl').css('border-right', `0px solid red`); // and right border property of #box_tl
+            if ($(this).hasClass('container')) {
                 $(this).css('border', `0px solid white`); //set outer container border property to white
             }
-        } else if (event.offsetX > ($('#box_tr').innerWidth() - 5)){
+        } else if (event.offsetX > ($('#box_tr').innerWidth() - 5)) {
             $('#box_tr').css('border-right', `10px solid red`);
         } else if (event.offsetY < 5) {
             $('#box_tr').css('border-top', `10px solid red`);
         } else if (event.offsetY > ($('#box_tr').innerHeight() - 5) || event.offsetY > ($('#box_tr').innerHeight() + 15)) {
-            $('#box_tr').css('border-bottom', `10px solid red`);
-            $('#box_bl').css('border-top', `0px solid red`);
-            $('#box_br').css('border-top', `0px solid red`);
+            $('#box_tr').css('border-bottom', `5px solid red`);
+            $('#box_br').css('border-top', `5px solid red`);
 
         }
-});    
+        console.log($(this));
+    });
 
 
-//bottom right box
-$('#box_br').click(function (event) {
+    //bottom right box
+    $('#box_br').click(function (event) {
 
-    if (event.offsetX < 5) { //if I click on the left side of #box_br
-        $('#box_br').css('border-left', `10px solid red`);//set left border property
-        $('#box_bl').css('border-right', `0px solid red`);// and right border property of #box_bl
-        if ($(this).hasClass('container')) { 
-            $(this).css('border', `0px solid white`); //set outer container border property to white
+        if (event.offsetX < 5) { //if I click on the left side of #box_br
+            $('#box_br').css('border-left', `10px solid red`); //set left border property
+            $('#box_bl').css('border-right', `0px solid red`); // and right border property of #box_bl
+            if ($(this).hasClass('container')) {
+                $(this).css('border', `0px solid white`); //set outer container border property to white
+            }
+        } else if (event.offsetX > ($('#box_br').innerWidth() - 5)) {
+            $('#box_br').css('border-right', `10px solid red`);
+        } else if (event.offsetY < 5) {
+            $('#box_br').css('border-top', `5px solid red`);
+            $('#box_tr').css('border-bottom', `5px solid red`);
+        } else if (event.offsetY > ($('#box_br').innerHeight() - 5) || event.offsetY > ($('#box_br').innerHeight() + 15)) {
+            $('#box_br').css('border-bottom', `10px solid red`);
+
         }
-    } else if (event.offsetX > ($('#box_br').innerWidth() - 5)){
-        $('#box_br').css('border-right', `10px solid red`);
-    } else if (event.offsetY < 5) {
-        $('#box_br').css('border-top', `5px solid red`);
-        $('#box_tr').css('border-bottom', `5px solid red`);
-    } else if (event.offsetY > ($('#box_br').innerHeight() - 5) || event.offsetY > ($('#box_br').innerHeight() + 15)) {
-        $('#box_br').css('border-bottom', `10px solid red`);
+        console.log($(this));
+    });
 
-    }
-});  
+    //bottom left box
+    $('#box_bl').click(function (event) {
+
+        if (event.offsetX < 5) { //if I click on the left side of #box_br
+            $('#box_bl').css('border-left', `10px solid red`); //set left border property
+            if ($(this).hasClass('container')) {
+                $(this).css('border', `0px solid white`); //set outer container border property to white
+            }
+        } else if (event.offsetX > ($('#box_bl').innerWidth() - 5)) {
+            $('#box_bl').css('border-right', `10px solid red`);
+            $('#box_br').css('border-left', `0px solid red`);
+        } else if (event.offsetY < 5) {
+            $('#box_bl').css('border-top', `5px solid red`);
+            $('#box_tl').css('border-bottom', `5px solid red`);
+        } else if (event.offsetY > ($('#box_bl').innerHeight() - 5) || event.offsetY > ($('#box_bl').innerHeight() + 15)) {
+            $('#box_bl').css('border-bottom', `10px solid red`);
+
+        }
+        console.log($(this));
+    });
 
 
-
-
+    //
 
 
 
 
     // $('#box_tr').click(function (event) {
-        
+
     //     if (event.offsetX < 50) {
     //         $(this).css('border-right', `10px solid red`);
     //         if ($(this).hasClass('container')) {
@@ -165,34 +231,34 @@ $('#box_br').click(function (event) {
 
     // })
 
-        
-                
-                // } else if (event.offsetY < 5) {
-                //     $('#box_tr').css('border-top', `10px solid red`);
-                //     if ($(this).hasClass('container')) {
-                //         $(this).css('border', `0px solid white`);
-                //     }
-                // }  
-                
-                // if (event.offsetX > ($('#box_tl').innerwidth()-5) || event.offsetX < ($('#box_tl').innerwidth()+10 )){
-                //     $('#box_tl').css('border-right', `10px solid red`);
-                // }
 
-        // if (event.offsetX > $(this).innerwidth()) {
-        //     $(this).css('border-right', `10px solid red`);
-        //     if ($(this).hasClass('container')) {
-        //         $(this).css('border', `0px solid white`);
-        //     }
-        // } if (event.offsetY < 5){
-        //     $(this).css('border-top', `10px solid red`);
-        //     if ($(this).hasClass('container')) {
-        //         $(this).css('border', `0px solid white`);
-        //     }
-            
-        // }
 
-       
-    
+    // } else if (event.offsetY < 5) {
+    //     $('#box_tr').css('border-top', `10px solid red`);
+    //     if ($(this).hasClass('container')) {
+    //         $(this).css('border', `0px solid white`);
+    //     }
+    // }  
+
+    // if (event.offsetX > ($('#box_tl').innerwidth()-5) || event.offsetX < ($('#box_tl').innerwidth()+10 )){
+    //     $('#box_tl').css('border-right', `10px solid red`);
+    // }
+
+    // if (event.offsetX > $(this).innerwidth()) {
+    //     $(this).css('border-right', `10px solid red`);
+    //     if ($(this).hasClass('container')) {
+    //         $(this).css('border', `0px solid white`);
+    //     }
+    // } if (event.offsetY < 5){
+    //     $(this).css('border-top', `10px solid red`);
+    //     if ($(this).hasClass('container')) {
+    //         $(this).css('border', `0px solid white`);
+    //     }
+
+    // }
+
+
+
     // another conditional to ignore the container 
 
 
